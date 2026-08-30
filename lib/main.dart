8c8687a9ec2c357bd9ccc64debd03e4aef82032b
Hà Nguyễn <trongha.dev@gmail.com>
2026-08-30 19:01:08 +0700

    feat: initialize multi-platform Flutter project structure for F-Cine

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      title: 'F-CINE',
+      title: 'F-Cine',
       theme: darkTheme,
       routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
